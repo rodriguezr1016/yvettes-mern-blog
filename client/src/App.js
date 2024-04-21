f2297c28c517fb5778f522357bc5fe9341d8107d
Rene Rodriguez <134916213+rodriguezr1016@users.noreply.github.com>
2024-04-21 21:47:30 +0000

    fuck you

--- client/src/App.js
+++ client/src/App.js
@@ -9,11 +9,9 @@ import { UserContextProvider } from './UserContext';
 import CreatePost from './pages/CreatePost';
 import PostPage from './pages/PostPage';
 import EditPost from './pages/EditPost';
-import {Analytics} from "@vercel/analytics"
 function App() {
   return (
     <UserContextProvider>
-      <Analytics />
       <Routes>
       <Route path='/' element={<Layout/>}>
         <Route index element={

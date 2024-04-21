9ea9d723338ab48be07a07eaecf260d71026b45a
Rene Rodriguez <134916213+rodriguezr1016@users.noreply.github.com>
2024-04-21 21:42:39 +0000

    analytics

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import { UserContextProvider } from './UserContext';
 import CreatePost from './pages/CreatePost';
 import PostPage from './pages/PostPage';
 import EditPost from './pages/EditPost';
-import { Analytics } from "@vercel/analytics/react"
+import {Analytics} from "@vercel/analytics"
 function App() {
   return (
     <UserContextProvider>
